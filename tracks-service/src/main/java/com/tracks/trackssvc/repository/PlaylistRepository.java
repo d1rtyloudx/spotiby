367f21155fe5a1dd6f9cf8ee6f2414733a9d33b6
ryhail <egor.zenkovich2005@gmail.com>
2025-03-09 21:40:58 +0300

    Repository

---
+++ tracks-service/src/main/java/com/tracks/trackssvc/repository/PlaylistRepository.java
@@ -0,0 +1,11 @@
+package com.tracks.trackssvc.repository;
+
+import com.tracks.trackssvc.model.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaylistRepository extends JpaRepository<Playlist, String> {
+
+}
